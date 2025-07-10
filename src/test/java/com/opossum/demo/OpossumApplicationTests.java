2728513d12b90de326b462d5c5e3ba66eec3224f
Imane <barbecheimane@gmail.com>
2025-07-10 15:19:41 +0200

    feat: Initial commit of Opossum project with MVP database schema and Spring Boot setup
    - Removed DBD SCHEMA.MD file containing the database schema for the MVP version.
    - Added .gitattributes and .gitignore files for project configuration.
    - Introduced Maven Wrapper files for easier Maven management.
    - Created pom.xml with Spring Boot dependencies and configurations.
    - Implemented main application class OpossumApplication.java.
    - Added application.properties for basic application configuration.
    - Included test class OpossumApplicationTests.java for context loading verification.

---
+++ src/test/java/com/opossum/demo/OpossumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opossum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpossumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
